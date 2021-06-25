71d3b002c7a787c56d6ccc04c4594d8eca091e11
BERMEO.HTML <57644310+AlissonBermeo@users.noreply.github.com>
2021-06-24 19:17:28 -0500

    Update cachematrix.R
    complemento de variable

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,14 @@ makeCacheMatrix <- function(x = matrix()) {
         m <<- NULL
             
 }
-
+   get <- function()x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m 
+    list(
+        set = set, get = get, 
+        setInverse = setInverse, 
+        getInverse = getInverse)
+   }        
 
 ## Write a short comment describing this function
 
